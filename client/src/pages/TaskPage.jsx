1467453bfd1c8c54619488f4c41cf59390a6a752
Anton <32438821+AntonNordgren@users.noreply.github.com>
2024-11-21 16:16:20 +0100

    More on Tasks

--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -7,6 +7,10 @@ function TaskPage() {
   const [title, setTitle] = useState("")
   const [addTitle, setAddTitle] = useState("")
   const [currentDelete, setCurrentDelete] = useState(null)
+  const [currentEdit, setCurrentEdit] = useState(null)
+  const [currentTask, setCurrentTask] = useState(null)
+
+  const [editTitle, setEditTitle] = useState("")
 
   useEffect(() => {
     console.log('Use Effect')
@@ -15,9 +19,13 @@ function TaskPage() {
 
   const handleEdit = (event, task) => {
 
-    console.log(task)
+    console.log("task id", task.id)
 
     setModalTitle(task.title)
+
+    setCurrentEdit(task.id)
+
+    setCurrentTask(task)
 
   }
 
@@ -32,11 +40,7 @@ function TaskPage() {
         ...tasks,
         {id: tasks.length, title: addTitle, done: false}
       ])
-
     }
-
-
-
   }
 
   const handleDelete = (event, task) => {
@@ -46,16 +50,35 @@ function TaskPage() {
     setCurrentDelete(task.id)
   }
 
-  const confirmDelete = (taskId) => {
+  const confirmDelete = () => {
 
+    console.log(currentDelete)
 
-    setTasks(tasks.filter(x => x.id !== taskId))
+    const newArray = tasks.filter(x => x.id !== currentDelete)
+
+    console.log(newArray)
+
+    setTasks(newArray)
 
   }
 
-  const submitEdit = (event) => {
+  const submitEdit = (event, task) => {
 
-    console.log(title)
+    console.log(currentTask)
+
+    currentTask.title = editTitle
+
+    let tasksCopy = [...tasks]
+
+    // tasksCopy[currentTask.id].title = editTitle
+
+    tasksCopy.splice(currentTask.id, 1, {id: currentTask.id, title: editTitle, done: false})
+
+    console.log(tasksCopy)
+
+    setTasks([
+      ...tasksCopy
+    ])
 
   }
 
@@ -69,7 +92,10 @@ function TaskPage() {
               <h3 className="text-start m-0">Your tasks</h3>
             </div>
             <div className="w-50 d-flex justify-content-end mx-3">
-              <button className="custom-button custom-button-green shadow" data-toggle="modal" data-target="#add-modal">Add</button>
+
+              {tasks.length !== 0 ? 
+                <button className="custom-button custom-button-green" data-toggle="modal" data-target="#add-modal">Add</button>
+              : null}
 
               <div className="modal fade text-black" id="add-modal" tabIndex="-1"
                 role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -90,7 +116,8 @@ function TaskPage() {
                     <div className="modal-body">
                       <div className="form-group">
                         <label for="exampleInputPassword1">Title</label>
-                        <input onChange={(event) => setAddTitle(event.target.value)} type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter new task title" />
+                        <input onChange={(event) => setAddTitle(event.target.value)} type="text" className="form-control" id="exampleInputPassword1"
+                        placeholder="Enter new task title" />
                       </div>
                       <div className="d-flex justify-content-end mt-4">
                         <button onClick={(event) => handleAdd(event)} data-dismiss="modal" type="button" className="btn btn-primary">Add</button>
@@ -104,6 +131,7 @@ function TaskPage() {
           </div>
           <table class="table table-dark m-auto">
             <tbody>
+
               {tasks.length !== 0 ? tasks.map(task => (
                 <tr key={task.id} className="h-100 align-items-center">
                   <td className="h-100 d-flex w-100 p-3">
@@ -137,7 +165,8 @@ function TaskPage() {
                               <button type="button" className="custom-button mx-2
                               custom-button-red px-3 justify-content-end"
                                 data-dismiss="modal" aria-label="Close">No</button>
-                              <button onClick={(event) => confirmDelete(task.id)} type="button" className="btn btn-primary " data-dismiss="modal" aria-label="Close">Yes</button>
+                              <button onClick={(event) => confirmDelete(event)}
+                              type="button" className="btn btn-primary " data-dismiss="modal" aria-label="Close">Yes</button>
                             </div>
 
                           </div>
@@ -164,10 +193,12 @@ function TaskPage() {
                           <div className="modal-body">
                             <div className="form-group">
                               <label for="exampleInputPassword1">Title</label>
-                              <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Edit title" />
+                              <input onChange={(event) => setEditTitle(event.target.value)}
+                              type="text" className="form-control" id="exampleInputPassword1" placeholder="Edit title" />
                             </div>
                             <div className="d-flex justify-content-end mt-4">
-                              <button onClick={(event) => submitEdit(event)} type="button" className="btn btn-primary">Edit</button>
+                              <button onClick={(event) => submitEdit(event, task)} type="button" className="btn btn-primary"
+                              data-dismiss="modal" aria-label="Close">Edit</button>
                             </div>
                             {/* <label type="text" value="Title" />
                       <input onChange={(event) => setTitle(event.target.value)} type="text" /> */}
@@ -179,7 +210,14 @@ function TaskPage() {
                   </td>
                 </tr>
 
-              )) : <>No Tasks</>}
+              )) :
+              <>
+                <h3 className="text-center">No Tasks</h3>
+                <div>
+                  <button className="custom-button custom-button-green shadow" data-toggle="modal" data-target="#add-modal">Add</button>
+                </div>
+              </>
+              }
             </tbody>
           </table>
         </div>
